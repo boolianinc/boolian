6e495621b0789b92b77ae161686b392774456cd6
Boolian Inc <boolianinc@gmail.com>
2011-11-29 15:37:33 -0700

    First time commit

---
+++ cygnus-web/src/main/webapp/WEB-INF/flows/readme.txt
@@ -0,0 +1,5 @@
+
+It is required to have a WEB-INF/flows directory.
+This file can be removed when some other files has been generated in the 
+flows directory.
+	
---
+++ setenv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export M2_HOME=/Users/Faisal/Data/HigherPlane/devtools/apache-maven-3.0.3
+export PATH=$M2_HOME/bin:$PATH
+export M2_REPO=/Users/Faisal/.m2/repository
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
+export MAVEN_OPTS="-Dfile.encoding=iso-8859-1 -Xms128m -Xmx1024m"
